7dc2f3bf44d347005606da18f08442cb3bb9b706
NguyenCuong32 <141486946+NguyenCuong32@users.noreply.github.com>
2023-09-15 08:35:04 +0700

    Init source code

---
+++ r2_component_function/src/Person.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+class Person extends React.Component{
+  render(){
+    return (
+      <h2>Hello World</h2>
+    );
+  }
+}
+export default Person;
